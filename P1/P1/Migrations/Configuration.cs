8c3ec5ba318c845770a8859e12bab95af7387e66
Kate <kychinsks.katya@gmail.com>
2021-01-09 12:58:02 +0200

    Second

---
+++ P1/P1/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace P1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<P1.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(P1.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ P1/P1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+namespace P1
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Latittude { get; set; }
+        public int Longitude { get; set; }
+    }
+
+    public class Address
+    {
+        [Required]
+        public string Street { get; set; }
+        public int CityId { get; set; }
+    }
+
+    public class Context : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<City> Cities { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().HasKey(x => x.Id);
+            modelBuilder.Entity<Person>().Property(x => x.Name).HasMaxLength(128);
+            modelBuilder.Entity<City>().HasKey(x => x.Id);
+        }
+
+    }
+    public class AdressRepository
+    {
+        public List<string> AllStreetsInCityWhereLiveMore10People(int cityId)
+        {
+            using (var ctx = new Context())
+            {
+                return ctx.Persons.Where(p=> p.Address.CityId== cityId).GroupBy(p=> p.Address.Street)
+                .Where(g => g.Count() > 10).Select(g => g.Key).ToList();
+            }
+        }
+     }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
